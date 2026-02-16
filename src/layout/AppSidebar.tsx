f38679a971fd4a46e0d428433e5421b3fc171666
Joel Mercedes <joelmercedes29@gmail.com>
2026-02-16 00:08:19 -0400

    update admin

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -271,11 +271,11 @@ const AppSidebar: React.FC = () => {
 
   return (
     <aside
-      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
+      className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-4 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
         ${isExpanded || isMobileOpen
-          ? "w-72.5"
+          ? "w-70"
           : isHovered
-            ? "w-72.5"
+            ? "w-70"
             : "w-22.5"
         }
         ${isMobileOpen ? "translate-x-0" : "-translate-x-full"}
@@ -284,7 +284,7 @@ const AppSidebar: React.FC = () => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <div
-        className={`py-8 flex  ${!isExpanded && !isHovered ? "lg:justify-center" : "justify-center"
+        className={`py-3 flex ${!isExpanded && !isHovered ? "lg:justify-center" : "justify-center"
           }`}
       >
         <Link href="/">
